389bbb4fa754653e802bde7d2160b9118911317f
smhrd <smhrd@192.168.219.63>
2024-12-23 11:32:23 +0900

    세영

--- Git/src/test2.java
+++ Git/src/test2.java
@@ -4,6 +4,10 @@ public class test2 {
 	public static void main(String[] args) {
 		System.out.println("새 클래스 만드러봄 브이");
 		System.out.println("또 추가해봄 브이브이");
+		
+		System.out.println("야호 num1 추가");
+		int num1=0;
+		
 
 	}
 
